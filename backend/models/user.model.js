cfa32189f8355d7f762c48cc6519eaf4cd24012f
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-05-13 19:44:18 +0200

    Added login, register routes and functionalities, JWT in backend server

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
   const Schema = mongoose.Schema;
 
   const usersSchema = new Schema({
-    fullname: String,
-    email:   String,
-    entries:  { type: Number, default: 0 },
+    fullname: {type : String},
+    email:   {type : String, required :true},
+    password : {type : String, required :true},
+    date : {type : Date, default: Date.now}
     
   });
 
