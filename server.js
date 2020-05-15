87934907c149815024ce87fed9b442237aca00b9
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-05-16 01:07:49 +0200

    rearranged the Client, Backend(server) structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -23,9 +24,19 @@ connection.once('open', () =>{
 
 const usersRouter= require('./routes/users');
 
-
+//Routes
 app.use('/users', usersRouter);
 
+//serve static assets if in production
+
+if(process.env.NODE_ENV === 'production'){
+	//set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*',(req,res)=>{
+		res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+			});
+}
 app.listen(port, () =>{
 	console.log(`server is running on port ${port}`);
 })
